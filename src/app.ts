515e7de5fc248b81bcf2a0760131432eadafae23
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-24 17:49:37 +0100

    Add express app

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+export const app = express();
+const corsOptions = {
+  origin: '*',
+};
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors(corsOptions));
+app.disable('x-powered-by');
+
+app.use((_req, _resp, next) => {
+  console.log('Soy un middleware');
+  next();
+});
