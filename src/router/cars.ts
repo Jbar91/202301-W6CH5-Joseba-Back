79a7b02d603cb19a7534d2aa70e07f2d93c51da7
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-02-25 13:41:15 +0100

    Add cars router

---
+++ src/router/cars.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { CarsController } from '../controllers/cars.js';
+import { CarsFileRepo } from '../repo/cars.file.repo.js';
+
+export const carsRouter = Router();
+const repo = new CarsFileRepo();
+const controller = new CarsController(repo);
+
+carsRouter.get('/', controller.getAll.bind(controller));
+carsRouter.get('/:id', controller.get.bind(controller));
+carsRouter.post('/', controller.post.bind(controller));
+carsRouter.patch('/:id', controller.patch.bind(controller));
+carsRouter.delete('/:id', controller.delete.bind(controller));
